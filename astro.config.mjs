cdcfed13b672930d4e0a2ecedbff6ffd5ef2b99c
somenoe <otashi24@gmail.com>
2024-03-15 02:04:31 +0700

    🔧 Set up PWA settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,29 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    AstroPWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#120f0f",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
